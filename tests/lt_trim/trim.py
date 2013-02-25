d9f884a1f95b08b58f54ebf40a14bb9767ab9b1d
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2013-02-25 09:28:24 +0100

    more tests

--- tests/lt_trim/trim.py
+++ tests/lt_trim/trim.py
@@ -30,17 +30,19 @@ def readUntilNull(proc):
 
     return ''.join(ret)
 
-class TestSimple(unittest.TestCase):
+class TestTrim(unittest.TestCase):
     def setUp(self):
         for cmd in ["../lttoolbox/lt-comp lr data/trim_mono.dix data/trim_mono.bin",
                     "../lttoolbox/lt-comp lr data/trim_bi.dix data/trim_bi.bin",
-                    "../lttoolbox/lt-trim data/trim_mono.bin data/trim_bi.bin data/trim_trimmed.bin"]:
+                    "../lttoolbox/lt-trim data/trim_mono.bin data/trim_bi.bin data/trim_trimmed.bin",
+                    "../lttoolbox/lt-comp lr data/trim_bi_regex.dix data/trim_bi_regex.bin",
+                    "../lttoolbox/lt-trim data/trim_mono.bin data/trim_bi_regex.bin data/trim_trimmed_regex.bin"]:
             exit_code, out = getstatusoutput(cmd)
             self.assertEqual(exit_code, 0,
                              msg="\n$ "+cmd+"\n"+out)
 
-    def in_out(self, insandouts):
-        proc = Popen(["../lttoolbox/lt-proc", "-e", "-z", "data/trim_trimmed.bin"],
+    def in_out(self, insandouts, dictionary):
+        proc = Popen(["../lttoolbox/lt-proc", "-e", "-z", dictionary],
                      stdin=PIPE,
                      stdout=PIPE,
                      stderr=PIPE)
@@ -59,12 +61,25 @@ class TestSimple(unittest.TestCase):
             [('ete', '^ete/ete<n><f><sg><ind>$'),
              ('løe', '^løe/løe<n><f><sg><ind>$'),
              ('æve', '^æve/æve<n><f><sg><ind>$'),
-             ('nobidixe', '^nobidixe/*nobidixe$')]
+             ('nobidixe', '^nobidixe/*nobidixe$')],
+            "data/trim_trimmed.bin"
+            )
+
+    def test_regex(self):
+        self.in_out(
+            [('ete', '^ete/ete<n><f><sg><ind>$'),
+             ('løe', '^løe/løe<n><f><sg><ind>$'),
+             ('æve', '^æve/æve<n><f><sg><ind>$'),
+             ('nobidixe', '^nobidixe/*nobidixe$'),
+             ('user\@foo.yes', '^user\@foo.yes/user\@foo.yes<email>$'),
+             ('user\@foo.no', '^user/*user$\@^foo/*foo$^./.<sent>$^maybe/*no$')],
+            "data/trim_trimmed_regex.bin"
             )
 
     def test_compounds_carry_over(self):
         self.in_out(
-            [('rivesemje', '^rivesemje/rive<n><f><sg><ind>+semje<n><f><sg><ind>$')]
+            [('rivesemje', '^rivesemje/rive<n><f><sg><ind>+semje<n><f><sg><ind>$')],
+            "data/trim_trimmed.bin"
             )
 
     def test_compounds_trim(self):
@@ -72,8 +87,6 @@ class TestSimple(unittest.TestCase):
             [('rive', '^rive/rive<n><f><sg><ind>$'),
              ('semje', '^semje/semje<n><f><sg><ind>$'),
              ('cmpnobidixe', '^cmpnobidixe/*cmpnobidixe$'),
-             ('tåkecmpnobidixe', '^tåkecmpnobidixe/*tåkecmpnobidixe$')]
+             ('tåkecmpnobidixe', '^tåkecmpnobidixe/*tåkecmpnobidixe$')],
+            "data/trim_trimmed.bin"
             )
-
-
-
